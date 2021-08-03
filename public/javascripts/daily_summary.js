f82185e11dd78177fa7d34df54b0448fafbc4f1c
Stin <krikarlss@gmail.com>
2021-08-03 23:40:32 +0200

    Add daily summary

---
+++ public/javascripts/daily_summary.js
@@ -0,0 +1,29 @@
+$(document).ready(() => {
+
+    var mymap = L.map('map').setView([62.56, 15.16], 5);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+
+    const endpoint = "/daily-summary/summary";
+    console.log("today?")
+    $.getJSON(endpoint, {})
+        .done((events) => {
+            events.forEach((event, idx) => {
+                let coords = `${event.location.gps}`.split(",");
+                let marker = L.marker([coords[0], coords[1]]).addTo(mymap).bindPopup(
+                    "<b>" + `${event.type}` + "</b>" + "<br>" +
+                    `${event.name}` + "<br><br>" +
+                    `${event.summary}` + "<br> <br>" +
+                    `Koordinater: ${event.location.gps}` + "<br>" +
+                    "Läs mer: <a href=https://www.polisen.se" + `${event.url}` + " target='_blank' "+  ">Polisen - aktuellt</a>"
+                );
+            });
+        })
+        .fail((xhr) => {
+            alert('Problem contacting server');
+            console.log(xhr);
+        });
+
+});
---
+++ routes/daily.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const got = require('got');
+const mongodb = require('mongodb');
+
+// Setup MongoDB
+const MongoClient = mongodb.MongoClient;
+const dbURL = "mongodb://localhost";
+
+console.log("IN DAILY *****************************************")
+router.get('/', function(req, res, next) {
+    res.render('daily', { title: 'Senaste dygnet' });
+});
+
+
+router.get('/summary', function(req, res, next) {
+
+    console.log("IDAG IDAG IDAG");
+    const datetime = new Date();
+    console.log(datetime.toISOString().substr(0, 10));
+    const currentDate = datetime.toISOString().substr(0, 10);
+
+    MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+        if (err) throw err;
+        else {
+            const db = client.db('happenings');
+            const events = db.collection('events');
+
+            events.find({datetime: {$gte: currentDate}}).project({}).toArray( (err, docs) => {
+                if (err) throw err;
+                else {
+                    res.send(docs);
+                }
+                client.close();
+            });
+
+        }
+    });
+});
+
+module.exports = router;
