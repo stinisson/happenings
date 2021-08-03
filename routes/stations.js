43170aada1f02259211d9543151a0f1cc7e06e63
Stin <krikarlss@gmail.com>
2021-08-03 15:27:27 +0200

    Update stations with cron job once every day

--- routes/stations.js
+++ routes/stations.js
@@ -2,45 +2,57 @@ const express = require('express');
 const router = express.Router();
 const got = require('got');
 const mongodb = require('mongodb');
+const CronJob = require('cron').CronJob;
+
+// Load stations once per day
+// cronTime, onTick, onComplete, start, timezone, context, runOnInit
+const job = new CronJob('0 0 0 * * *', saveStations, null, true, null, null, true);
 
 // Setup MongoDB
 const MongoClient = mongodb.MongoClient;
 const dbURL = "mongodb://localhost";
 
-
 function saveStations() {
-    MongoClient.connect(dbURL), {useUnifiedTopology: true}, (err, client) => {
-        if (err) throw err;
-        else {
-            console.log("hej");
-            const db = client.db('happenings');
-            const stations = db.collection('policeStations');
-        }
-    }
-}
-
-
-async function cacheStations() {
-    // https://polisen.se/api/policestations
-    //const stations = await got('https://jsonplaceholder.typicode.com/todosz/1').json();
-    const stations = await got('https://jsonplaceholder.typicode.com/todos/1', {responseType: 'json'}).then(response => {
-        return response.body;
+    got('https://polisen.se/api/policestations', {responseType: 'json'}).then(response => {
+        MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+            if (err) throw err;
+            else {
+                const db = client.db('happenings');
+                db.collection("policeStations").insertMany(response, function(err, res) {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+                    client.close();
+                });
+            }
+        })
     }).catch(error => {
         console.log(error);
     });
-    return stations;
 }
 
-router.get('/', async function(req, res) {
+
+router.get('/', function(req, res) {
     res.render('stations', { title: 'Nyheter och händelser' });
-    const stations = await cacheStations();
-    console.log(stations);
 });
 
 router.get('/data', function(req, res, next) {
-    res.send({'hej': "hej", "hej2": "hh"});
-});
+    MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+        if (err) throw err;
+        else {
+            const db = client.db('happenings');
+            const policeStations = db.collection('policeStations');
 
+            policeStations.find({}).project({id: 1, name: 1, Url: 1, location: 1, services: 1}).toArray( (err, docs) => {
+                if (err) throw err;
+                else {
+                    res.send(docs);
+                }
+                client.close();
+            });
+
+        }
+    });
+});
 
 module.exports = router;
 
