f817680a3a5cfddad5a51a50203b77ef61daa4e5
Stin <krikarlss@gmail.com>
2021-08-03 22:11:50 +0200

    Save events

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,57 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const got = require('got');
+const mongodb = require('mongodb');
+
+// Setup MongoDB
+const MongoClient = mongodb.MongoClient;
+const dbURL = "mongodb://localhost";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Nyheter och händelser' });
+  saveEvents();
+});
+
+function saveEvents() {
+  got('https://polisen.se/api/events', {responseType: 'json'}).then(response => {
+
+    MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+      if (err) throw err;
+      else {
+        const db = client.db('happenings');
+        db.collection("events").deleteMany({}).then(r =>
+            db.collection("events").insertMany(response.body, function(err, res) {
+              if (err) throw err;
+              console.log("1 document inserted");
+              client.close();
+            })
+        );
+      }
+    })
+  }).catch(error => {
+    console.log(error);
+  });
+}
+
+router.get('/data', function(req, res, next) {
+  MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) throw err;
+    else {
+      const db = client.db('happenings');
+      const events = db.collection('events');
+
+      events.find({}).project({}).toArray( (err, docs) => {
+        if (err) throw err;
+        else {
+          res.send(docs);
+        }
+        client.close();
+      });
+
+    }
+  });
 });
 
 module.exports = router;
