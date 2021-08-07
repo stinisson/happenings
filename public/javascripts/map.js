a9f70944737617e7a6a6cf0b0de9233235dd293f
Stin <krikarlss@gmail.com>
2021-08-07 23:10:51 +0200

    Add event category overlay filtering

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,74 +1,7 @@
-//import {eventsLayer} from "./utils.js";
-
 $(document).ready(() => {
 
-    // Leaflet map
-    //const mymap = L.map('map', {minZoom: 5}).setView([62.56, 15.16], 5);
-
-    var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
-        denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
-        aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
-        golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
-
-    var cities = L.layerGroup([littleton, denver, aurora, golden]);
-
-
-    const mymap = L.map('map', {
-        center: [62.56, 15.16],
-        zoom: 5,
-        minZoom: 5,
-        layers: [cities]
-    });
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
-
-    // Locate me icon
-    const locateMeIcon = new L.Icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
-
-    // Locate me-button
-    L.easyButton({
-        states:[
-            {
-                stateName: 'unloaded',
-                icon: 'fa-location-arrow',
-                title: 'Visa din plats',
-                onClick: function(control){
-                    control.state("loading");
-                    control._map.on('locationfound', function(e){
-                        L.marker(e.latlng, {icon: locateMeIcon}).addTo(control._map).bindPopup("<b>Du är här!</b><br>").openPopup();
-                        this.setView(e.latlng, 13);
-                        control.state('loaded');
-                    });
-                    control._map.on('locationerror', function(){
-                        control.state('error');
-                    });
-                    control._map.locate()
-                }
-            }, {
-                stateName: 'loading',
-                icon: 'fa-spinner fa-spin'
-            }, {
-                stateName: 'loaded',
-                icon: 'fa-thumbs-up'
-            }, {
-                stateName: 'error',
-                icon: 'fa-frown-o',
-                title: 'location not found'
-            }
-        ]
-    }).addTo(mymap);
-
-    // Events
-    var icon = new L.Icon({
+    // Events icons
+    const icon = new L.Icon({
         iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
         iconSize: [25, 41],
@@ -77,134 +10,149 @@ $(document).ready(() => {
         shadowSize: [41, 41]
     });
 
-    const eventsLayer = {
-        'Alkohollagen': cities,
-        'Anträffad död': cities};
-/*
+    function createMap(layers) {
+        const mymap = L.map('map', {
+            center: [62.56, 15.16],
+            zoom: 5,
+            minZoom: 5,
+            layers: layers
+        });
 
-    const eventsLayer = {
-        'Alkohollagen': cities,
-        'Anträffad död': cities,
-        'Anträffat gods': cities,
-        'Arbetsplatsolycka': cities,
-        'Bedrägeri': cities,
-        'Bombhot': cities,
-        'Brand': cities,
-        'Brand automatlarm': cities,
-        'Bråk': cities,
-        'Detonation': cities,
-        'Djur skadat/omhändertaget': cities,
-        'Ekobrott': cities,
-        'Farligt föremål, misstänkt': cities,
-        'Fjällräddning': cities,
-        'Fylleri/LOB': cities,
-        'Förfalskningsbrott': cities,
-        'Försvunnen person': cities,
-        'Gränskontroll': cities,
-        'Häleri': cities,
-        'Inbrott': cities,
-        'Inbrott, försök': cities,
-        'Knivlagen': cities,
-        'Kontroll person/fordon': cities,
-        'Lagen om hundar och katter': cities,
-        'Larm inbrott': cities,
-        'Larm överfall': cities,
-        'Miljöbrott': cities,
-        'Missbruk av urkund': cities,
-        'Misshandel': cities,
-        'Misshandel, grov': cities,
-        'Mord/dråp': cities,
-        'Mord/dråp, försök': cities,
-        'Motorfordon, anträffat stulet': cities,
-        'Motorfordon, stöld': cities,
-        'Narkotikabrott': cities,
-        'Naturkatastrof': cities,
-        'Ofog barn/ungdom': cities,
-        'Ofredande/förargelse': cities,
-        'Olaga frihetsberövande': cities,
-        'Olaga hot': cities,
-        'Olaga intrång/hemfridsbrott': cities,
-        'Olovlig körning': cities,
-        'Ordningslagen': cities,
-        'Polisinsats/kommendering': cities,
-        'Rattfylleri': cities,
-        'Räddningsinsats': cities,
-        'Rån': cities,
-        'Rån väpnat': cities,
-        'Rån övrigt': cities,
-        'Rån, försök': cities,
-        'Sammanfattning dag': cities,
-        'Sammanfattning dygn': cities,
-        'Sammanfattning eftermiddag': cities,
-        'Sammanfattning förmiddag': cities,
-        'Sammanfattning helg': cities,
-        'Sammanfattning kväll': cities,
-        'Sammanfattning kväll och natt': cities,
-        'Sammanfattning natt': cities,
-        'Sammanfattning vecka': cities,
-        'Sedlighetsbrott': cities,
-        'Sjukdom/olycksfall': cities,
-        'Sjölagen': cities,
-        'Skadegörelse': cities,
-        'Skottlossning': cities,
-        'Skottlossning, misstänkt': cities,
-        'Spridning smittsamma kemikalier': cities,
-        'Stöld': cities,
-        'Stöld, försök': cities,
-        'Stöld, ringa': cities,
-        'Stöld/inbrott': cities,
-        'Tillfälligt obemannat': cities,
-        'Trafikbrott': cities,
-        'Trafikhinder': cities,
-        'Trafikkontroll': cities,
-        'Trafikolycka': cities,
-        'Trafikolycka, personskada': cities,
-        'Trafikolycka, singel': cities,
-        'Trafikolycka, smitning från': cities,
-        'Trafikolycka, vilt': cities,
-        'Uppdatering': cities,
-        'Utlänningslagen': cities,
-        'Vapenlagen': cities,
-        'Varningslarm/haveri': cities,
-        'Våld/hot mot tjänsteman': cities,
-        'Våldtäkt': cities,
-        'Våldtäkt, försök': cities,
-        'Vållande till kroppsskada': cities
-    };*/
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
 
-    L.control.layers(null, eventsLayer, {collapsed:false, sortLayers:true,
+        // Locate me icon
+        const locateMeIcon = new L.Icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
 
-        onClick: function(event){
+        // Locate me-button
+        L.easyButton({
+            states:[
+                {
+                    stateName: 'unloaded',
+                    icon: 'fa-location-arrow',
+                    title: 'Visa din plats',
+                    onClick: function(control){
+                        control.state("loading");
+                        control._map.on('locationfound', function(e){
+                            L.marker(e.latlng, {icon: locateMeIcon}).addTo(control._map).bindPopup("<b>Du är här!</b><br>").openPopup();
+                            this.setView(e.latlng, 13);
+                            control.state('loaded');
+                        });
+                        control._map.on('locationerror', function(){
+                            control.state('error');
+                        });
+                        control._map.locate()
+                    }
+                }, {
+                    stateName: 'loading',
+                    icon: 'fa-spinner fa-spin'
+                }, {
+                    stateName: 'loaded',
+                    icon: 'fa-thumbs-up'
+                }, {
+                    stateName: 'error',
+                    icon: 'fa-frown-o',
+                    title: 'location not found'
+                }
+            ]
+        }).addTo(mymap);
 
-            var id = event.currentTarget.layerId;
-            var layer = this._layers[id];
-            console.log(layer);
-            console.log(id)
+        return mymap;
+    }
 
-        },
-
-        },
-
-
-        ).addTo(mymap);
-    $(".leaflet-control-layers-overlays").parent().prepend('<div class="control-title"><b>Händelser</b></div>');
-
+    // Retrieve event data
     const endpoint = "/data";
     $.getJSON(endpoint, {})
         .done((events) => {
+            let indeterminate = false;
+            let eventLayers = {};
+            let eventControlLayers = [];
             events.forEach((event, idx) => {
+
                 let coords = `${event.location.gps}`.split(",");
-                let marker = L.marker([coords[0], coords[1]], {icon: icon}).addTo(mymap).bindPopup(
+                let newEvent = L.marker([coords[0], coords[1]], {icon: icon}).bindPopup(
                     "<b>" + `${event.type}` + "</b>" + "<br>" +
                     `${event.name}` + "<br><br>" +
                     `${event.summary}` + "<br> <br>" +
                     `Koordinater: ${event.location.gps}` + "<br>" +
                     "Läs mer: <a href=https://www.polisen.se" + `${event.url}` + " target='_blank' "+  ">Polisen - aktuellt</a>"
                 );
+
+                if (!eventLayers.hasOwnProperty(`${event.type}`)) {
+                    const newLayerGroup = L.layerGroup();
+                    eventLayers[`${event.type}`] = newLayerGroup;
+                    eventControlLayers.push(newLayerGroup);
+                }
+                eventLayers[`${event.type}`].addLayer(newEvent);
+
             });
+
+            const mymap = createMap(eventControlLayers);
+
+            L.control.layers(null, eventLayers, {collapsed:false, sortLayers:true}).addTo(mymap);
+            $(".leaflet-control-layers-overlays").parent().prepend('<div class="control-title"><b>Händelser</b></div>');
+
+            $(".leaflet-control-layers-overlays").prepend('<span><div><input type="checkbox" id="control-events" checked=""><span> Alla händelser</span></div></span>');
+
+            const layerSelectorUpdate = function() {
+                const overall = document.querySelector('input[id="control-events"]');
+                const events = document.querySelectorAll('input[class="leaflet-control-layers-selector"]');
+
+                let checkedCount = 0;
+                for(let i = 0; i < events.length; i++) {
+                    if(events[i].checked) {
+                        checkedCount++;
+                    }
+                }
+
+                if (checkedCount === 0) {
+                    overall.checked = false;
+                    overall.indeterminate = false;
+                } else if (checkedCount === events.length) {
+                    overall.checked = true;
+                    overall.indeterminate = false;
+                } else {
+                    overall.checked = false;
+                    overall.indeterminate = true;
+                }
+                indeterminate = overall.indeterminate;
+            }
+
+            $(".leaflet-control-layers-selector").change(layerSelectorUpdate);
+
+            $("#control-events").change(function() {
+                const overall = document.querySelector('input[id="control-events"]');
+                const events = document.querySelectorAll('input[class="leaflet-control-layers-selector"]');
+
+                if (!indeterminate && overall.checked) {
+                    for(let i = 0; i < events.length; i++) {
+                        if(!events[i].checked) {
+                            events[i].click();
+                        }
+                    }
+                } else {
+                    for(let i = 0; i < events.length; i++) {
+                        if(events[i].checked) {
+                            events[i].click();
+                        }
+                    }
+                }
+                indeterminate = overall.indeterminate;
+            });
+
         })
         .fail((xhr) => {
             alert('Problem contacting server');
             console.log(xhr);
+            const mymap = createMap([]);
         });
+
 });
