e9f48da3981e1bda14c66830833f93b02815ac0d
Stin <krikarlss@gmail.com>
2021-08-10 17:57:25 +0200

    Start on station control filtering

--- public/javascripts/station_map.js
+++ public/javascripts/station_map.js
@@ -1,3 +1,139 @@
+import {getEventData} from "./common.js";
+
+// Leaflet map
+function createMap() {
+    const mymap = L.map('map', {
+        center: [62.56, 15.16],
+        zoom: 5,
+        minZoom: 5,
+        //layers: layers
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+
+    // Locate me icon
+    const locateMeIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    // Locate me-button
+    L.easyButton({
+        states:[
+            {
+                stateName: 'unloaded',
+                icon: 'fa-location-arrow',
+                title: 'Visa din plats',
+                onClick: function(control){
+                    control.state("loading");
+                    control._map.on('locationfound', function(e){
+                        L.marker(e.latlng, {icon: locateMeIcon}).addTo(control._map).bindPopup("<b>Du är här!</b><br>").openPopup();
+                        this.setView(e.latlng, 13);
+                        control.state('loaded');
+                    });
+                    control._map.on('locationerror', function(){
+                        control.state('error');
+                    });
+                    control._map.locate()
+                }
+            }, {
+                stateName: 'loading',
+                icon: 'fa-spinner fa-spin'
+            }, {
+                stateName: 'loaded',
+                icon: 'fa-thumbs-up'
+            }, {
+                stateName: 'error',
+                icon: 'fa-frown-o',
+                title: 'Platsen hittades inte'
+            }
+        ]
+    }).addTo(mymap);
+
+    return mymap;
+}
+
+$(document).ready(() => {
+
+    const eventIconBlue = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    var overlayMaps = ['Anmälan', 'Pass'];
+/*        'Anmälan': cities,
+        'Pass ': cities,
+        'Tillstånd ': cities,
+        'Hittegods ': cities,
+        'Vapen ': cities,
+        'Delgivning ': cities,
+        'Cyklar  ': cities,
+        'Provisoriskt pass': cities,
+        'Beslag ': cities*/
+
+
+    const endpoint = "/police-stations/data";
+    $.getJSON(endpoint, {})
+        .done((events) => {
+
+
+            let stations = [];
+
+            const mymap = createMap();
+
+            events.forEach((event, idx) => {
+
+                let coords = `${events[idx].location.gps}`.split(",");
+                let services = "<ul class='stationService'>";
+
+                events[idx].services.forEach(function (service, i) {
+                    services += '<li>' + service.name + '</li>';
+                });
+                services += "</ul>";
+
+                let newEvent = L.marker([coords[0], coords[1]], {icon: eventIconBlue}).bindPopup(
+                    `Polisstation: ${event.name}` + "<br>" +
+                    `Adress: ${event.location.name}` + "<br>" +
+                    `GPS: ${event.location.gps}` + "<br>" +
+                    "Tjänster: " + services +
+                    "Öppettider och tjänster: <a href=" + `${event.Url}` + " target='_blank'>Polisen - kontakt</a>"
+                );
+
+                stations.push( {marker: newEvent, open: true, services: ["Pass"]} );
+
+
+
+            });
+
+
+            var cities = L.layerGroup([]);
+            cities.addTo(mymap);
+
+
+
+        })
+        .fail((xhr) => {
+            alert('Problem contacting server');
+            console.log(xhr);
+            const mymap = createMap([]);
+        });
+
+
+});
+
+
+
+/*
 
 $(document).ready(() => {
 
@@ -103,4 +239,4 @@ $(document).ready(() => {
             console.log(xhr);
         });
 
-});
+});*/
