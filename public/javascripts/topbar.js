e4c545a230dfce87392e2e503ad27ac9b9b653ff
Stin <krikarlss@gmail.com>
2021-08-14 00:07:20 +0200

    Fix responsive navbar and update order of nav tabs

---
+++ public/javascripts/topbar.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+  var x = document.getElementById("Topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
