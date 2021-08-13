711014e84573ac94d44160cb954e89612ca29bce
Stin <krikarlss@gmail.com>
2021-08-13 19:59:47 +0200

    Add statistics from 2017 to 2019

--- public/javascripts/crime_statistics.js
+++ public/javascripts/crime_statistics.js
@@ -1,11 +1,12 @@
-import {config_shootings_2019, config_shootings_2020, config_shootings_2021} from './statistics_data.js';
-import {config_explosions_2019, config_explosions_2020, config_explosions_2021} from './statistics_data.js';
-import {data_explosions_2019, data_explosions_2020, data_explosions_2021} from './statistics_data.js';
+import {config_shootings_2017, config_shootings_2018, config_shootings_2019, config_shootings_2020, config_shootings_2021} from './statistics_data.js';
+import {config_explosions_2018, config_explosions_2019, config_explosions_2020, config_explosions_2021} from './statistics_data.js';
+import {data_explosions_2018, data_explosions_2019, data_explosions_2020, data_explosions_2021} from './statistics_data.js';
 
 // Make
 let explosions_2021;
 let explosions_2020;
 let explosions_2019;
+let explosions_2018;
 
 function setupExplosionData() {
     setTimeout(function() {
@@ -15,6 +16,8 @@ function setupExplosionData() {
         explosions_2020.update();
         explosions_2019.data = data_explosions_2019;
         explosions_2019.update();
+        explosions_2018.data = data_explosions_2018;
+        explosions_2018.update();
     }, 250);
 }
 window.setupExplosionData = setupExplosionData;
@@ -25,7 +28,6 @@ $(document).ready(() => {
         e.preventDefault();
         $(this).tab('show');
     })
-
 
     // Shootings
     const shootings_2021 = new Chart(
@@ -43,20 +45,35 @@ $(document).ready(() => {
         config_shootings_2019
     );
 
+    const shootings_2018 = new Chart(
+      document.getElementById('shootings_2018'),
+      config_shootings_2018
+    );
+
+    const shootings_2017 = new Chart(
+      document.getElementById('shootings_2017'),
+      config_shootings_2017
+    );
+
     // Explosions
-    explosions_2019 = new Chart(
-        document.getElementById('explosions_2019'),
-        config_explosions_2019
+    explosions_2021 = new Chart(
+      document.getElementById('explosions_2021'),
+      config_explosions_2021
     );
 
     explosions_2020 = new Chart(
-        document.getElementById('explosions_2020'),
-        config_explosions_2020
+      document.getElementById('explosions_2020'),
+      config_explosions_2020
     );
 
-    explosions_2021 = new Chart(
-        document.getElementById('explosions_2021'),
-        config_explosions_2021
+    explosions_2019 = new Chart(
+      document.getElementById('explosions_2019'),
+      config_explosions_2019
+    );
+
+    explosions_2018 = new Chart(
+      document.getElementById('explosions_2018'),
+      config_explosions_2018
     );
 
 });
--- public/javascripts/statistics_data.js
+++ public/javascripts/statistics_data.js
@@ -1,4 +1,4 @@
-import {chartColors, chartColorsTransparent, labels, options, options_explosions} from "./utils.js";
+import {chartColors, chartColorsTransparent, labels, options, options_explosions} from "./chart_utils.js";
 
 const data_skjutningar_2021 = [22, 14, 21, 21, 34, 34, 34, 0, 0, 0, 0, 0];
 const data_avlidna_2021 = [2, 3, 1, 3, 8, 4, 4, 0, 0, 0, 0, 0];
@@ -12,6 +12,14 @@ const data_skjutningar_2019 = [17, 23, 17, 24, 29, 45, 27, 32, 35, 18, 30, 37];
 const data_avlidna_2019 = [3, 5, 1, 6, 1, 4, 2, 4, 1, 4, 1, 10];
 const data_skadade_2019 = [5, 4, 7, 8, 12, 17, 11, 11, 12, 4, 11, 18];
 
+const data_skjutningar_2018 = [21, 14, 20, 21, 31, 25, 32, 26, 32, 26, 32, 26];
+const data_avlidna_2018 = [6, 2, 0, 1, 6, 8, 7, 1, 3, 3, 6, 2];
+const data_skadade_2018 = [11, 2, 12, 9, 14, 17, 13, 11, 13, 9, 19, 5];
+
+const data_skjutningar_2017 = [33, 19, 28, 19, 22, 26, 33, 28, 31, 29, 24, 32];
+const data_avlidna_2017 = [4, 2, 8, 2, 4, 6, 1, 3, 1, 4, 6, 2];
+const data_skadade_2017 = [10, 10, 8, 6, 6, 9, 24, 17, 16, 14, 7, 12];
+
 
 export const data_detonationer_2021 = [9, 5, 9, 8, 11, 6, 3, 0, 0, 0, 0, 0];
 export const data_forberedelse_2021 = [3, 7, 9, 5, 5, 4, 6, 0, 0, 0, 0, 0];
@@ -24,6 +32,10 @@ export const data_forsok_2020 = [3, 2, 1, 1, 0, 0, 0, 0, 2, 4, 0, 0];
 export const data_detonationer_2019 = [18, 6, 6, 10, 6, 11, 8, 5, 14, 16, 22, 11];
 export const data_forberedelse_2019 = [2, 5, 5, 9, 8, 4, 5, 10, 9, 9, 9, 7];
 export const data_forsok_2019 = [0, 0, 1, 4, 1, 0, 3, 5, 6, 1, 3, 3];
+
+export const data_detonationer_2018 = [6, 7, 3, 4, 8, 7, 6, 9, 6, 5, 11, 18];
+export const data_forberedelse_2018 = [3, 7, 5, 4, 4, 5, 3, 6, 2, 5, 6, 2];
+export const data_forsok_2018 = [2, 2, 1, 2, 1, 1, 0, 0, 3, 0, 0, 0];
 
 // Shootings
 const data_shootings_2021 = {
@@ -100,6 +112,60 @@ const data_shootings_2019 = {
         {
             label: 'Skadade 2019',
             data: data_skadade_2019,
+            borderColor: chartColors.orange,
+            backgroundColor: chartColorsTransparent.orange,
+            yAxisID: 'y',
+        }
+    ]
+};
+
+const data_shootings_2018 = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'Skjutningar 2018',
+            data: data_skjutningar_2018,
+            borderColor: chartColors.yellow,
+            backgroundColor: chartColorsTransparent.yellow,
+            yAxisID: 'y',
+        },
+        {
+            label: 'Avlidna 2018',
+            data: data_avlidna_2018,
+            borderColor: chartColors.red,
+            backgroundColor: chartColorsTransparent.red,
+            yAxisID: 'y',
+        },
+        {
+            label: 'Skadade 2018',
+            data: data_skadade_2018,
+            borderColor: chartColors.orange,
+            backgroundColor: chartColorsTransparent.orange,
+            yAxisID: 'y',
+        }
+    ]
+};
+
+const data_shootings_2017 = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'Skjutningar 2017',
+            data: data_skjutningar_2017,
+            borderColor: chartColors.yellow,
+            backgroundColor: chartColorsTransparent.yellow,
+            yAxisID: 'y',
+        },
+        {
+            label: 'Avlidna 2017',
+            data: data_avlidna_2017,
+            borderColor: chartColors.red,
+            backgroundColor: chartColorsTransparent.red,
+            yAxisID: 'y',
+        },
+        {
+            label: 'Skadade 2017',
+            data: data_skadade_2017,
             borderColor: chartColors.orange,
             backgroundColor: chartColorsTransparent.orange,
             yAxisID: 'y',
@@ -185,7 +251,34 @@ export const data_explosions_2019 = {
             borderColor: chartColors.yellow,
             backgroundColor: chartColorsTransparent.yellow,
             yAxisID: 'y',
+        }
+    ]
+};
+
+export const data_explosions_2018 = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'Detonationer 2018',
+            data: data_detonationer_2018,
+            borderColor: chartColors.red,
+            backgroundColor: chartColorsTransparent.red,
+            yAxisID: 'y',
         },
+        {
+            label: 'Försök 2018',
+            data: data_forsok_2018,
+            borderColor: chartColors.orange,
+            backgroundColor: chartColorsTransparent.orange,
+            yAxisID: 'y',
+        },
+        {
+            label: 'Förberedelse 2018',
+            data: data_forberedelse_2018,
+            borderColor: chartColors.yellow,
+            backgroundColor: chartColorsTransparent.yellow,
+            yAxisID: 'y',
+        }
     ]
 };
 
@@ -208,6 +301,18 @@ export const config_shootings_2019 = {
     options: options
 };
 
+export const config_shootings_2018 = {
+    type: 'line',
+    data: data_shootings_2018,
+    options: options
+};
+
+export const config_shootings_2017 = {
+    type: 'line',
+    data: data_shootings_2017,
+    options: options
+};
+
 // Explosions
 export const config_explosions_2021 = {
     type: 'bar',
@@ -224,6 +329,12 @@ export const config_explosions_2020 = {
 export const config_explosions_2019 = {
     type: 'bar',
     //data: data_explosions_2019,
+    options: options_explosions
+};
+
+export const config_explosions_2018 = {
+    type: 'bar',
+    //data: data_explosions_2018,
     options: options_explosions
 };
 
